6a63ad63a9978fd4ce8f690d73d7fa04602b2215
nicsoftarg <NiSci1984>
2018-07-10 17:28:30 -0300

    file

---
+++ Publicis.DDM.Middleware/Models/Client.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Publicis.DDM.Middleware.Models
+{
+ 
+    public class Client
+	{
+		[BsonId]
+		public int ClientId
+		{
+			get;
+			set;
+		}
+
+		[BsonElement("name")]
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		[BsonElement("Values")]
+		public Dictionary<string, object> Values
+		{
+			get;
+			set;
+		}
+	}
+}
