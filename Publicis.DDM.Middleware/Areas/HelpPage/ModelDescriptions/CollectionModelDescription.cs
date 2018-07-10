61dc267fb72e4c56319193605714dd79d225a472
Román Gorojovsky <rgoro@southplans.com.ar>
2018-07-10 16:50:54 -0300

    Proyecto backend

---
+++ Publicis.DDM.Middleware/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Publicis.DDM.Middleware.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
