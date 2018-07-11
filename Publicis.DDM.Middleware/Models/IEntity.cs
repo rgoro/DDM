664222743937911e5350f17ec63bea53f4499529
nicsoftarg <NiSci1984>
2018-07-11 10:22:17 -0300

    IEntity Added

---
+++ Publicis.DDM.Middleware/Models/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Publicis.DDM.Middleware.Models
+{
+	public interface IEntity
+	{
+		string EntityName { get; set; }
+	}
+}
